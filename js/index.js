27fa1b3c0c7693927e4827e43754835ca7df73f7
averylawton <lawton.avery@gmail.com>
2021-04-23 19:57:24 -0500

    Updated personal website html, css, js, and image.

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+function updateDates() {
+
+    const d = new Date();
+
+    const currentYear = d.getFullYear();
+
+    const modified = document.lastModified
+
+    document.getElementById('lastmod').textContent = modified;
+    document.querySelector('#currentYear').textContent = currentYear;
+}
+
+window.onload = updateDates;
+
+
+
+
+
