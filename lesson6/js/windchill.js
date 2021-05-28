568fdc46e4de9b816f990398142ef9916f246fe3
averylawton <lawton.avery@gmail.com>
2021-05-28 17:50:06 -0500

    Updates for lesson 6 and 7

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -23,16 +23,20 @@ function windChill(tempF, speed){
 
     //f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
     
-    return (35.74 + (.6215*tempF))-(35.75*(Math.pow(speed,.16))) + (.4275*(tempF*(Math.pow(speed,.16))));
+    return Math.round((35.74 + (.6215*tempF))-(35.75*(Math.pow(speed,.16))) + (.4275*(tempF*(Math.pow(speed,.16)))));
     
     
     }
     
-        function doInputOutput(){
-    let temp = parseInt(document.getElementById('averagetemp').value);
-    let wind = parseInt(document.getElementById('windSpeed').value);
-    
-    document.getElementById('windChill').innerHTML =
-              windChill(temp, wind);
-    
-        }
+
+    let temp = parseInt(document.getElementById('currentTemp').innerText);
+
+    let wind = parseInt(document.getElementById('windSpeed').innerText);
+
+    // Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) 
+    // and wind speeds above 4.8 kilometers per hour (3.0 mph).
+    if (temp <= 50 && wind > 3) {
+        document.getElementById('windChill').innerHTML = windChill(temp, wind);
+    }
+    else 
+        document.getElementById('windChill').innerHTML = "-";
