95b287d7c7e09067b3bca4ceab108704171ddd27
averylawton <lawton.avery@gmail.com>
2021-06-11 21:02:19 -0500

    lesson 8 final changes b4 submit

--- lesson8/js/stormcenter.js
+++ lesson8/js/stormcenter.js
@@ -1,3 +1,23 @@
+
+function doOnload() {
+
+    const d = new Date();
+    const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(d);
+    document.getElementById("date").innerHTML = `${fulldate}`;
+
+    const menu1button = document.querySelector('.menu1');
+    const mainnav = document.querySelector('.navigation');
+    
+    menu1button.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+}
+//________________________________________________________________________________________________________//
+window.onload = doOnload;
+
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+
+
