84f62f002179208e5e7994bd524fa1d25d0e4911
averylawton <lawton.avery@gmail.com>
2021-06-29 13:59:22 -0500

    index changes

--- lesson11/js/gallery-7.js
+++ lesson11/js/gallery-7.js
@@ -32,23 +32,3 @@ const imgObserver = new IntersectionObserver((entries,
 imgs.forEach(image => {
 	imgObserver.observe(image);
 });
-
-
-
-//_________________________________________________________________________________________________________//
-
-if (localStorage) {
-    var lastVisitMessage = "";
-    var lastVisit = localStorage.getItem('lastVisit');
-    if (lastVisit == null) {
-        lastVisitMessage = "This is your first time to our site! Welcome!";
-    }
-    else {
-        // do some calculations to determine number of days since last visit
-        var todaysDate = new Date();
-        var numberOfDays = Math.round((todaysDate.getTime() - lastVisit) / (1000*60*60*24));
-        lastVisitMessage = "Your last visit to our site was " + numberOfDays + " days ago."; 
-    } 
-  
-    
-  }
